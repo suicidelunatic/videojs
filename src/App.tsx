9fd64fbc33c02d22a4ef13a799eabea98aa434b5
Vladislav Gapchinsky <>
2021-09-16 13:54:42 +0300

    update transcriptions/styles/labels

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,13 @@ const src = `${process.env.REACT_APP_URL}/2.P_13.4_ForceandMotionQC_1.m3u8`;
 const transcriptons: VideoTypes.Transcription[] = [
   {
     id: "english",
-    src: `${process.env.REACT_APP_URL}/force_and_motion_en.vtt`,
-    label: "english",
+    src: `${process.env.REACT_APP_URL}/english.vtt`,
+    label: "English",
   },
   {
     id: "alternative",
     src: `${process.env.REACT_APP_URL}/Force_and_Motion_(Alternative).vtt`,
-    label: "alternative en",
-  },
-  {
-    id: "spanish",
-    src: `${process.env.REACT_APP_URL}/force_and_motion_spa.vtt`,
-    label: "spanish",
+    label: "Alternative EN",
   },
 ];
 const App: React.FC = () => {
